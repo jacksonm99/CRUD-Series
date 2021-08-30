86daa1a994ff17c5891a71016051a826373d8862
Jackson Silva <jacksonmanoel1999@gmail.com>
2021-08-29 21:29:00 -0300

    Adiciona outras categorias de séries

--- Enums/Genero.cs
+++ Enums/Genero.cs
@@ -2,13 +2,25 @@ namespace CadastroSeries
 {
     public enum Genero
     {
-        Aventura = 1,
+        
+    Aventura = 1,
+    Ação = 2,
+    Comédia = 3,
+    Dança= 4,
+    Documentário = 5,
+    Drama = 6,
+    Espionagem = 7,
+    Fantasia = 8,
+    Faroeste= 9,
+    Ficção científica = 10,
+    Guerra = 11,
+    Mistério = 12,
+    Musical = 13,
+    Romance = 14,
+    Terror = 15,
+       
+        
 
-        Ação = 2,
-        Terror = 3,
-        Comédia = 4,
-        romance = 5,
-        Drama = 6,
     }
 
 }
